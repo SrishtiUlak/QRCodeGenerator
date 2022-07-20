15e6d6bdc2fc676ec065de023e6216cb8fe40c98
UlakS <srishti@codebee.dk>
2022-07-20 09:41:39 +0545

    Adding QRCodeGenerator

---
+++ QRGenerate/QRcode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRCodeGenerator
+{
+    public partial class QRcode : Form
+    {
+        public QRcode()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCoder.QRCodeGenerator QG = new QRCoder.QRCodeGenerator();
+            var data = QG.CreateQrCode(textBox1.Text,QRCoder.QRCodeGenerator.ECCLevel.H);
+            var code = new QRCoder.QRCode(data);
+            pictureBox1.Image = code.GetGraphic(50);
+        }
+    }
+}
